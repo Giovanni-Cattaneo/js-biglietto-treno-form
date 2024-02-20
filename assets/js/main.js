ec1fbd91beaaaa856c14b577bb3073bed13e578e
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-02-20 16:20:11 +0100

    dd form in html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 console.log("hello");
 
-const userAge = document.getElementById("age");
+const userAge = document.getElementById("age").value;
 
-const userDistance = document.getElementById("distance");
+const userDistance = document.getElementById("distance").value;
 
 console.log (Number(userAge), Number(userDistance));
 
@@ -11,7 +11,7 @@ const button = document.getElementById("btn");
 button.addEventListener("click", myFunction)
 
 function myFunction(){
-    console.log(Number(userAge.value), Number(userDistance.value));
+    console.log(Number(userAge), Number(userDistance));
 }
 
 if ( userAge.value < 18){
