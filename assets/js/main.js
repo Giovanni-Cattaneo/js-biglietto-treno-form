e6c23269ec42728a2ea3fac806c19eb3d5dde296
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-02-20 15:30:01 +0100

    add btn in js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,11 @@
 console.log("hello");
 
-const userAge = document.getElementById("age")
-const userDistance = document.getElementById("distance")
+const userAge = document.getElementById("age");
+const userDistance = document.getElementById("distance");
 
 console.log(userAge);
+console.log(userDistance);
+
+const button = document.getElementById("btn");
+
+console.log(button);
