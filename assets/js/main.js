67fb846a086ebf539335d7f110441a209f6c6c37
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-02-20 17:16:16 +0100

    add input name

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,9 +4,15 @@ const inputAge = document.getElementById("age"); // input età
 
 const inputDistance = document.getElementById("distance"); // input distanza
 
+const inputIdentity = document.getElementById("identity")
+
 const button = document.getElementById("btn");
 
 button.addEventListener("click", myFunction)
+
+let juniorDiscount = 0.8
+
+let seniorDiscount = 0.6
 
 function myFunction(){
     console.log (Number(inputAge.value)) ; // valore età
@@ -16,14 +22,14 @@ function myFunction(){
     let tripPrice = userDistance * 0.21;
 
     if (userAge < 18) {
-        tripPrice *= 0.8
+        tripPrice *= juniorDiscount
     } else if (userAge > 65){
-        tripPrice *= 0.6
+        tripPrice *= seniorDiscount
     }
 
     tripPrice = tripPrice.toFixed(2);
 
-    console.log(tripPrice);
+    console.log(identity.value, tripPrice);
 
     document.getElementById("price").innerHTML = tripPrice;
 }
