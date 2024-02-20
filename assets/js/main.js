c100dfd1b4a5a77e234b583ce28b76a88501b553
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-02-20 15:54:05 +0100

    edit function

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,13 @@ const userAge = document.getElementById("age");
 
 const userDistance = document.getElementById("distance");
 
-console.log (Number(userAge), Number (userDistance));
+console.log (Number(userAge), Number(userDistance));
 
 const button = document.getElementById("btn");
 
 button.addEventListener("click", myFunction)
 
 function myFunction(){
-    console.log(Number(userAge.value), Number (userDistance.value));
+    console.log(Number(userAge.value), Number(userDistance.value));
 }
+
